b022c3408f1eac6bd8c05ba83b33f1c116f1484f
Steven O'Driscoll <slodriscoll@gmail.com>
2024-12-12 16:39:01 -0700

    more updates

--- webfont-kit.sh
+++ webfont-kit.sh
@@ -6,30 +6,29 @@ SOURCE=$1
 DESTINATION=$2
 
 type=${SOURCE: -4}
-if [[ $type==".ttf" ]]; then
-  echo "equals"
+
+# we omit mime check since we use this script only locally and assume it's otf
+# if [[ ! "$type" =~ ^(".otf"|".ttf"|".eot"|"woff"|"off2")$ ]]; then
+#   echo "usage: convert_otf requires an OTF file."
+#   echo "usage: convert_otf input.otf /path/to/output/"
+#   exit 1
+# fi
+
+if [[ $type == ".otf" ]]; then
+  echo "otf equals $type"
   exit 0
 fi
 
-# we omit mime check since we use this script only locally and assume it's otf
-if [[ "$type" != "(.otf|.ttf|.eot|woff|off2)" ]]; then
-  echo "usage: convert_otf requires an OTF file."
-  echo "usage: convert_otf input.otf /path/to/output/"
-  exit 1
-fi
-
-
-
-if [[ "$type" == ".otf" ]]; then
-  /bin/bash convert_otf.sh
-elif [[ "$type" == ".ttf" ]]; then
-  /bin/bash convert_ttf.sh
-elif [[ "$type" == ".eot" ]]; then
-  /bin/bash convert_eot.sh
-elif [[ ${SOURCE: -5} == ".woff" ]]; then
-  /bin/bash convert_woff.sh
-elif [[ ${SOURCE: -6} == ".woff2" ]]; then
-  /bin/bash convert_woff2.sh
+if [[ $type == ".otf" ]]; then
+  /bin/bash ~/bash_files/bash_scripts/Webfont-Kit/convert_otf.sh
+elif [[ $type == ".ttf" ]]; then
+  /bin/bash ~/bash_files/bash_scripts/Webfont-Kit/convert_ttf.sh
+# elif [[ "$type" == ".eot" ]]; then
+#   /bin/bash ./convert_eot.sh
+# elif [[ ${SOURCE: -5} == ".woff" ]]; then
+#   /bin/bash ./convert_woff.sh
+# elif [[ ${SOURCE: -6} == ".woff2" ]]; then
+#   /bin/bash convert_woff2.sh
 else
   echo "I can only convert one of the following file types"
   echo '".otf", ".ttf", ".eot", ".woff", ".woff2"'
