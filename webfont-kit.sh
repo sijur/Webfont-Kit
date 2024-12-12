2d689ef4a4c8791540de50867d785f81db04c9d7
Steven O'Driscoll <slodriscoll@gmail.com>
2024-12-12 16:29:32 -0700

    updates to the files

--- webfont-kit.sh
+++ webfont-kit.sh
@@ -6,19 +6,25 @@ SOURCE=$1
 DESTINATION=$2
 
 type=${SOURCE: -4}
+if [[ $type==".ttf" ]]; then
+  echo "equals"
+  exit 0
+fi
 
 # we omit mime check since we use this script only locally and assume it's otf
-if [[ $type != "(.otf|.ttf|.eot|woff|off2)" ]]; then
+if [[ "$type" != "(.otf|.ttf|.eot|woff|off2)" ]]; then
   echo "usage: convert_otf requires an OTF file."
   echo "usage: convert_otf input.otf /path/to/output/"
   exit 1
 fi
 
-if [[ $type == ".otf" ]]; then
+
+
+if [[ "$type" == ".otf" ]]; then
   /bin/bash convert_otf.sh
-elif [[ $type == ".ttf" ]]; then
+elif [[ "$type" == ".ttf" ]]; then
   /bin/bash convert_ttf.sh
-elif [[ $type == ".eot" ]]; then
+elif [[ "$type" == ".eot" ]]; then
   /bin/bash convert_eot.sh
 elif [[ ${SOURCE: -5} == ".woff" ]]; then
   /bin/bash convert_woff.sh
